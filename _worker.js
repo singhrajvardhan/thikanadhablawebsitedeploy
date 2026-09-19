2c862b988e82695c3819876098e5fb6b269baaf4
Thikana Dhabla <thikanadhabla@gmail.com>
2026-09-19 18:18:27 +0530

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,13 @@
+export default {
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+
+    if (response.status === 404) {
+      return env.ASSETS.fetch(
+        new Request(new URL("/index.html", request.url))
+      );
+    }
+
+    return response;
+  },
+};
